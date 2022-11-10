a47a95640e506492e733c01d59b3a5efc462ce38
samuel <94730955+sameharu@users.noreply.github.com>
2022-11-10 14:44:37 -0600

    2nd cut

--- apartment-tenants-registration-webapp/js/index.js
+++ apartment-tenants-registration-webapp/js/index.js
@@ -2,23 +2,71 @@
 
 window.onload = function(){
 
-const registration = document.getElementById("mainContent") ;
-
-registration.onsubmit = function(event){
-
-          event.defaultPrevented() ; 
-
-
-          const buildingNumber = document.getElementById("buildingNo")
-          const FloorNo =  document.getElementById()
-
-          const appartmentNumber = document.getElementById()
-
-
-         
-
-
-}
-
-
-}
+          const registration = document.getElementById("all") ;
+          
+          registration.onsubmit = function(event){
+          
+                    event.defaultPrevented() ; 
+          
+          
+                    const buildingNumber = document.getElementById("buildingNo")
+                    const FloorNo =  document.getElementById("floorNo")
+          
+                    const appartmentNumber = document.getElementById("apartmentNo")
+          
+          
+          
+                    const title = document.getElementById("title") ;
+          
+                    const fullName = document.getElementById("fullNames") ;
+          
+                    const phoneNumber = document.getElementById("phoneNo") ;
+          
+                    const email = document.getElementById("email") ;
+          
+                    const driverNumber = document.getElementById("driversLicenceNo") ;
+          
+          
+          
+                    const titleMain = title.value ;
+          
+                    const fullNameMain = fullName.value ;
+          
+                    const phoneNumberMain  =  phoneNumber.value ; 
+          
+                    const emailMain = email.value ; 
+          
+                    const driverNumberMain = driverNumber.value ; 
+          
+          
+                    console.log(titleMain) ;
+          
+          
+          
+                    titleMain.focus() ;
+          
+          
+          
+          
+          
+                    
+          
+          
+          
+                    
+                    
+                    
+          
+          
+          
+          
+          
+                   
+          
+          
+          }
+          
+          
+          }
+          
+          console.log("hello")
