cdda77bd9113afbe9ef643b666ef7daef4065ea0
samuel <94730955+sameharu@users.noreply.github.com>
2022-11-10 14:21:35 -0600

    first cut

---
+++ apartment-tenants-registration-webapp/js/index.js
@@ -0,0 +1,24 @@
+
+
+window.onload = function(){
+
+const registration = document.getElementById("mainContent") ;
+
+registration.onsubmit = function(event){
+
+          event.defaultPrevented() ; 
+
+
+          const buildingNumber = document.getElementById("buildingNo")
+          const FloorNo =  document.getElementById()
+
+          const appartmentNumber = document.getElementById()
+
+
+         
+
+
+}
+
+
+}
